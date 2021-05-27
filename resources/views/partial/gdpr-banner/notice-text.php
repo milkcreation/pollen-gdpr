9ffd6880136938385bbc9b0ae1b6a4264b534434
Jordy Manner <jordy.manner@milkcreation.fr>
2021-05-27 06:20:42 +0000

    gdpr >> 1.1.x-dev

--- resources/views/partial/gdpr-banner/notice-text.php
+++ resources/views/partial/gdpr-banner/notice-text.php
@@ -4,9 +4,6 @@
  */
 ?>
 <div class="GdprBanner-noticeText">
-    <?php _e(
-        'En poursuivant votre navigation sur ce site, vous acceptez l’utilisation de cookies pour vous proposer ' .
-        'des services et offres adaptés à vos centres d’intérêts.',
-        'pollen-gdpr'
-    ); ?>
+    <?php echo 'En poursuivant votre navigation sur ce site, vous acceptez l’utilisation de cookies pour vous ' .
+        'proposer des services et offres adaptés à vos centres d’intérêts.'; ?>
 </div>
